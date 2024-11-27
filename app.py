2c805b675dcd0d5db3da20017f026d77800866cd
andreev-sergej <andreevsergej1111@gmail.com>
2024-11-28 00:53:23 +0300

    Test1

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__, template_folder='templates')
+cors = CORS(app)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
   
